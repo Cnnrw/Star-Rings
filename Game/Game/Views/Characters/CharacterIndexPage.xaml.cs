c7229a6b88b43434126e93d3f114c1c35d6ef7f7
Tate <tatebrasel@gmail.com>
2021-01-31 12:42:57 -0800

    Added CharacterIndexPage to project

---
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -0,0 +1,62 @@
+using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)] 
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterIndexPage : ContentPage
+    {
+        // The view model, used for data binding
+        readonly ItemIndexViewModel ViewModel = ItemIndexViewModel.Instance;
+
+        // Empty Constructor for UTs
+        public CharacterIndexPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the CharacterIndexView Model
+        /// </summary>
+        public CharacterIndexPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ViewModel;
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
+        }
+    }
+}
