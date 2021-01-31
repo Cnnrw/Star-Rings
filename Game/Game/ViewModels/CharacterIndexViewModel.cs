1032bf46df5315c919fd36a38765c14078fc3107
Tate <tatebrasel@gmail.com>
2021-01-31 13:01:56 -0800

    Added the CharacterIndexViewModel

---
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+using Game.Models;
+using Game.Views;
+using Game.GameRules;
+
+namespace Game.ViewModels
+{
+    /// <summary>
+    /// Index View Model
+    /// Manages the list of data records
+    /// </summary>
+    public class CharacterIndexViewModel : BaseViewModel<CharacterModel>
+    {
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile CharacterIndexViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static CharacterIndexViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new CharacterIndexViewModel();
+                            instance.Initialize();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// 
+        /// The constructor subscribes message listeners for crudi operations
+        /// </summary>
+        public CharacterIndexViewModel()
+        {
+            Title = "Characters";
+
+            #region Messages
+
+            #endregion Messages
+        }
+
+        #endregion Constructor
+        
+        #region DataOperations_CRUDi
+
+        /// <summary>
+        /// Returns the Score passed in
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public CharacterModel CheckIfCharacterExists(CharacterModel data)
+        {
+            // This will walk the Scores and find if there is one that is the same.
+            // If so, it returns the Score...
+
+            var myList = Dataset.Where(a =>
+                                        a.Name == data.Name)
+                                        .FirstOrDefault();
+
+            if (myList == null)
+            {
+                // it's not a match, return false;
+                return null;
+            }
+
+            return myList;
+        }
+
+        /// <summary>
+        /// Load the Default Data
+        /// </summary>
+        /// <returns></returns>
+        public override List<CharacterModel> GetDefaultData() 
+        {
+            return DefaultData.LoadData(new CharacterModel());
+        }
+
+        #endregion DataOperations_CRUDi
+
+        #region SortDataSet
+
+        /// <summary>
+        /// The Sort Order for the ScoreModel
+        /// </summary>
+        /// <param name="dataset"></param>
+        /// <returns></returns>
+        public override List<CharacterModel> SortDataset(List<CharacterModel> dataset)
+        {
+            return dataset
+                    .OrderBy(a => a.Name)
+                    .ThenBy(a => a.Description)
+                    .ToList();
+        }
+
+        #endregion SortDataSet
+    }
+}
