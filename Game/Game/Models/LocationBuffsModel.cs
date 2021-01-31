fe21bf7908d42aed61e6af235af2e2fc45f79a2f
Tate <tatebrasel@gmail.com>
2021-01-31 14:53:41 -0800

    Added LocationBuffsModel

---
+++ Game/Game/Models/LocationBuffsModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models
+{
+    public class LocationBuffsModel
+    {
+        public int AttackBuffValue; 
+        public int DefenseBuffValue;
+        public int SpeedBuffValue;
+    }
+}
