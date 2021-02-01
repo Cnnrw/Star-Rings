196fdefde3e66238fcc17eb14a37e38a69bf851c
Tate <tatebrasel@gmail.com>
2021-01-31 23:29:55 -0800

    Added JediCharacterModel to Project
    (cherry picked from commit ccf4b4b25e9ac592367de6b122ea7213d7888fcb)

---
+++ Game/Game/Models/JediCharacterModel.cs
@@ -0,0 +1,113 @@
+using System.Collections.Generic;
+
+using Game.GameRules;
+
+namespace Game.Models
+{
+    /// <summary>
+    /// Jedi Characters
+    /// 
+    /// Derive from CharacterModel
+    /// </summary>
+    public class JediCharacterModel : CharacterModel
+    {
+        /// <summary>
+        /// Default Jedi character
+        /// 
+        /// Gets a type, guid, name and description
+        /// </summary>
+        public JediCharacterModel()
+        {
+            PlayerType = PlayerTypeEnum.Character;
+            Guid = Id;
+            Name = "Luke Skywalker";
+            Description = "A Jedi Master and leader in the Rebel Alliance";
+            Level = 1;
+            ImageURI = "item.png";
+            ExperienceTotal = 0;
+            ExperienceRemaining = LevelTableHelper.LevelDetailsList[Level + 1].Experience - 1;
+
+            // Default to Jedi job
+            Job = CharacterJobEnum.Jedi;
+
+            LocationBuffs = new Dictionary<string, LocationBuffsModel>();
+        }
+
+        /// <summary>
+        /// Create a copy
+        /// </summary>
+        /// <param name="data"></param>
+        public JediCharacterModel(CharacterModel data)
+        {
+            Update(data);
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="newData"></param>
+        /// <returns></returns>
+        public bool Update(JediCharacterModel newData)
+        {
+            if (newData == null)
+            {
+                return false;
+            }
+
+            PlayerType = newData.PlayerType;
+            Guid = newData.Guid;
+            Name = newData.Name;
+            Description = newData.Description;
+            Level = newData.Level;
+            ImageURI = newData.ImageURI;
+
+            // Difficulty = newData.Difficulty;
+
+            Speed = newData.Speed;
+            Defense = newData.Defense;
+            Attack = newData.Attack;
+
+            ExperienceTotal = newData.ExperienceTotal;
+            ExperienceRemaining = newData.ExperienceRemaining;
+            CurrentHealth = newData.CurrentHealth;
+            MaxHealth = newData.MaxHealth;
+
+            Head = newData.Head;
+            Necklass = newData.Necklass;
+            PrimaryHand = newData.PrimaryHand;
+            OffHand = newData.OffHand;
+            RightFinger = newData.RightFinger;
+            LeftFinger = newData.LeftFinger;
+            Feet = newData.Feet;
+            UniqueItem = newData.UniqueItem;
+
+            // Update the Job
+            Job = newData.Job;
+
+            LocationBuffs = newData.LocationBuffs;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Helper to combine the attributes into a single line, to make it easier to display the item as a string
+        /// </summary>
+        /// <returns></returns>
+        public override string FormatOutput()
+        {
+            var myReturn = string.Empty;
+            myReturn += Name;
+            myReturn += " , " + Description;
+            myReturn += " , a " + Job.ToMessage();
+            myReturn += " , Level : " + Level.ToString();
+            myReturn += " , Total Experience : " + ExperienceTotal;
+            myReturn += " , Attack :" + GetAttackTotal;
+            myReturn += " , Defense :" + GetDefenseTotal;
+            myReturn += " , Speed :" + GetSpeedTotal;
+            myReturn += " , Items : " + ItemSlotsFormatOutput();
+            myReturn += " , Damage : " + GetDamageTotalString;
+
+            return myReturn;
+        }
+    }
+}
