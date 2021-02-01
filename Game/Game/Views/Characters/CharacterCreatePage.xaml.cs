25dc916792345d5260127e8b71bcc6faacc352d9
Connor Wilding <cnnr@cnnr.io>
2021-02-01 11:05:36 -0800

    Added CharacterCreatePage to project, changed CharacterCreatePage.xaml, CharacterCreatePage.xaml.cs, Game.csproj
    (cherry picked from commit dc22bb4eb153053081bb4b195d0aa9eb982f5e06)

---
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -0,0 +1,57 @@
+using System;
+using System.ComponentModel;
+
+using Game.Models;
+using Game.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+    /// <summary>
+    ///     Create a character
+    /// </summary>
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterCreatePage : ContentPage
+    {
+        // The Character to create
+        private readonly GenericViewModel<CharacterModel> ViewModel = new GenericViewModel<CharacterModel>();
+
+        // Empty Constructor for UTs
+        public CharacterCreatePage(bool unitTest) { }
+
+        /// <summary>
+        ///     Constructor for Create makes a new model
+        /// </summary>
+        public CharacterCreatePage()
+        {
+            InitializeComponent();
+
+            ViewModel.Data = new CharacterModel();
+
+            BindingContext = ViewModel;
+
+            ViewModel.Title = "Create";
+        }
+
+        /// <summary>
+        ///     Save by calling for Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "Create", ViewModel.Data);
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        ///     Cancel the Create
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Cancel_Clicked(object sender, EventArgs e) => await Navigation.PopModalAsync();
+    }
+}
