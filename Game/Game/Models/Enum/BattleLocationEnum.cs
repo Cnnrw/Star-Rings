5062b735bdc3559974e54b88d929f6a8e55f2cc7
Tate <tatebrasel@gmail.com>
2021-01-31 21:52:53 -0800

    Added BattleLocationEnum.cs to Project

---
+++ Game/Game/Models/Enum/BattleLocationEnum.cs
@@ -0,0 +1,64 @@
+namespace Game.Models
+{
+    /// <summary>
+    /// The Types of location a battle can take place in.
+    /// Used in Battles.
+    /// </summary>
+    public enum BattleLocationEnum
+    {
+        // Not specified
+        Unknown = 0,
+
+        Shire = 1,
+
+        ElvenCity = 2,
+
+        Forest = 3,
+
+        Dungeons = 4,
+
+        Mordor = 5,
+    }
+
+    /// <summary>
+    /// Friendly strings for the Enum Class
+    /// </summary>
+    public static class BattleLocationEnumExtensions
+    {
+        /// <summary>
+        /// Display a String for the Enums
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToString(this BattleLocationEnum value)
+        {
+            // Default String
+            var Result = "Battle location";
+
+            switch (value)
+            {
+                case BattleLocationEnum.Shire:
+                    Result = "The Shire";
+                    break;
+
+                case BattleLocationEnum.ElvenCity:
+                    Result = "Elven City";
+                    break;
+
+                case BattleLocationEnum.Forest:
+                    Result = "Forest";
+                    break;
+
+                case BattleLocationEnum.Dungeons:
+                    Result = "Dungeons";
+                    break;
+
+                case BattleLocationEnum.Mordor:
+                    Result = "Mordor";
+                    break;
+            }
+
+            return Result;
+        }
+    }
+}
