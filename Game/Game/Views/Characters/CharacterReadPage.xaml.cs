efbc1dfabc2e42901d8018cb0997df8ba0ebd486
Tate <tatebrasel@gmail.com>
2021-02-01 22:22:28 -0800

    Added navigation from CharacterReadPage to CharacterUpdatePage, in CharacterReadPage.cs
    (cherry picked from commit 959219206b660a028708f27a8021710021719f08)

--- Game/Game/Views/Characters/CharacterReadPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterReadPage.xaml.cs
@@ -52,8 +52,8 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void Delete_Clicked(object sender, EventArgs e)
         {
-            //await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(ViewModel)));
-            //await Navigation.PopAsync();
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterDeletePage(ViewModel)));
+            await Navigation.PopAsync();
         }
     }
 }
