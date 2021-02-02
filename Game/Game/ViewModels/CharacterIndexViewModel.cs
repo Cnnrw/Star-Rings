f7f57d9b45fecbd52aeb90217728099bcf912218
Tate <tatebrasel@gmail.com>
2021-02-01 21:15:25 -0800

    Added subscription for Character Create message to CharacterIndexViewModel.cs
    (cherry picked from commit dd6f6886341e1e1c659d32aaaded36839fe29e48)

--- Game/Game/ViewModels/CharacterIndexViewModel.cs
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -57,6 +57,12 @@ namespace Game.ViewModels
 
             #region Messages
 
+            // Register the Create Message
+            MessagingCenter.Subscribe<CharacterCreatePage, CharacterModel>(this, "Create", async (obj, data) =>
+            {
+                await CreateAsync(data as CharacterModel);
+            });
+
             #endregion Messages
         }
 
