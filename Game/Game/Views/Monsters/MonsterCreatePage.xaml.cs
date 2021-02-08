71805151131351040e4afd503343cb486350cc25
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-08 10:35:36 -0800

    Add documentation to method implementations in MonsterCreatePage.xaml.cs
    (cherry picked from commit 2cdb0430d5e6d266fbc05efb5c2915e58742075e)

--- Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -6,21 +6,29 @@ using Game.Models;
 using Game.ViewModels;
 
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Game.Views
 {
     /// <summary>
-    ///     Create a character
+    ///     Monster create page
     /// </summary>
     [DesignTimeVisible(false)]
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MonsterCreatePage : ContentPage
     {
-        // The Character to create
-        private readonly GenericViewModel<MonsterModel> ViewModel = new GenericViewModel<MonsterModel>();
+        /// <summary>
+        ///     Internal Monster ViewModel
+        /// </summary>
+        private readonly GenericViewModel<MonsterModel> _viewModel = new GenericViewModel<MonsterModel>();
 
-        // Empty Constructor for UTs
+        /// <summary>
+        ///     PopupLocationEnum holds the current location selected
+        /// </summary>
+        public ItemLocationEnum PopupLocationEnum = ItemLocationEnum.Unknown;
+
+        /// <summary>
+        ///     Empty Constructor for UTs
+        /// </summary>
+        /// <param name="unitTest"></param>
         public MonsterCreatePage(bool unitTest) { }
 
         /// <summary>
@@ -30,27 +38,35 @@ namespace Game.Views
         {
             InitializeComponent();
 
-            ViewModel.Data = new MonsterModel();
+            _viewModel.Data = RandomPlayerHelper.GetRandomMonster(1);
+            _viewModel.Title = _viewModel.Data.Name;
 
-            BindingContext = ViewModel;
-
-            ViewModel.Title = "Create";
+            BindingContext = _viewModel;
         }
 
-        void OnAttackStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            AttackValueLabel.Text = String.Format("{0}", e.NewValue);
-        }
+        /// <summary>
+        ///     Changes Attack attribute of a Monster
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnAttackStepperValueChanged(object sender, ValueChangedEventArgs e) =>
+            AttackValueLabel.Text = $"{e.NewValue}";
 
-        void OnDefenseStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            DefenseValueLabel.Text = String.Format("{0}", e.NewValue);
-        }
+        /// <summary>
+        ///     Changes defense attribute of a Monster
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnDefenseStepperValueChanged(object sender, ValueChangedEventArgs e) =>
+            DefenseValueLabel.Text = $"{e.NewValue}";
 
-        void OnSpeedStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            SpeedValueLabel.Text = String.Format("{0}", e.NewValue);
-        }
+        /// <summary>
+        ///     Changes Speed attribute of a Monster
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnSpeedStepperValueChanged(object sender, ValueChangedEventArgs e) =>
+            SpeedValueLabel.Text = $"{e.NewValue}";
 
         /// <summary>
         ///     Save by calling for Create
@@ -59,7 +75,12 @@ namespace Game.Views
         /// <param name="e"></param>
         private async void Save_Clicked(object sender, EventArgs e)
         {
-            MessagingCenter.Send(this, "Create", ViewModel.Data);
+            if (string.IsNullOrEmpty(_viewModel.Data.ImageURI))
+            {
+                _viewModel.Data.ImageURI = new MonsterModel().ImageURI;
+            }
+
+            MessagingCenter.Send(this, "Create", _viewModel.Data);
             await Navigation.PopModalAsync();
         }
 
