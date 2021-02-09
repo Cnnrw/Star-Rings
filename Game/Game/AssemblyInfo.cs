d2f570f96cfad81780a7b3e2039f675f35734bd5
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-09 13:38:05 -0800

    Add ExportFont attribute for each font in AssemblyInfo.cs
    I put the attributes in AssemblyInfo.cs so the fonts are visible globally. "gamefont" is aliased to "8bit", "gamefont_bold" to "8bit_bold", and "gamefont_small" to "8bit_small".
    
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/AssemblyInfo.cs
+++ Game/Game/AssemblyInfo.cs
@@ -1,3 +1,7 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("gamefont.tff", Alias = "8bit")]
+[assembly: ExportFont("gamefont_bold.tff", Alias = "8bit_bold")]
+[assembly: ExportFont("gamefont_small.tff", Alias = "8bit_small")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
