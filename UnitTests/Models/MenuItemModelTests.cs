b28710f437aaf62ae5776f944ef4bf17e7bab474
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-11 13:08:46 -0800

    Renames HomeMenuItemModelTests to MenuItemModelTests
    Changes to: MenuItemModelTests.cs
    
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

---
+++ UnitTests/Models/MenuItemModelTests.cs
@@ -0,0 +1,35 @@
+﻿using Game.Models;
+
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class MenuItemModelTests
+    {
+        [Test]
+        public void MenuItemModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new MenuItemModel();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void MenuItemModel_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var result = new MenuItemModel {Id = MenuItemEnum.Village, Title = "bogus title"};
+
+            // The Get is tested by retrieving it here as well.
+            Assert.AreEqual("bogus title", result.Title);
+            Assert.AreEqual(MenuItemEnum.Village, result.Id);
+        }
+    }
+}
