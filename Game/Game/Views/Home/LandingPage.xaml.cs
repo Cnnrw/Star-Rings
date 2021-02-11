10f48cfe90e1b5683c55c50e155b098c27588d35
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-11 12:37:14 -0800

    Renames HomePage to LandingPage
    Changes to: LandingPage.xaml, LandingPage.xaml.cs
    
    This is more so a landing page than it was a home page.
    
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

---
+++ Game/Game/Views/Home/LandingPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views
+{
+    /// <summary>
+    ///     The first page a users sees
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LandingPage : ContentPage
+    {
+        /// <summary>
+        ///     Constructor
+        /// </summary>
+        public LandingPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///     Example of a Button Click (this one is Sync, if calling Async then needs to be Async)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void EnterButton_Clicked(object sender, EventArgs e) => await Navigation.PushAsync(new HomePage());
+    }
+}
