45411f20d156b4d19497146b96019d69905126fe
Tate <tatebrasel@gmail.com>
2021-02-10 23:28:15 -0800

    Added alert to stop the user from updating Characters to have no name. Changes in CharacterUpdatePage.xaml.cs

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -83,8 +83,15 @@ namespace Game.Views
         /// <param name="e"></param>
         private async void Save_Clicked(object sender, EventArgs e)
         {
-            MessagingCenter.Send(this, "Update", ViewModel.Data);
-            await Navigation.PopModalAsync();
+            if (ViewModel.Data.Name.Length == 0)
+            {
+                await DisplayAlert("Hold up!", "Please give your character a name", "OK");
+            }
+            else
+            {
+                MessagingCenter.Send(this, "Update", ViewModel.Data);
+                await Navigation.PopModalAsync();
+            }
         }
 
         /// <summary>
