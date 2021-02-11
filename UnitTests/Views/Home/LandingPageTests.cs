b36763170a13e6fb7de5e51ea248457553464caf
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-11 12:42:31 -0800

    Renames HomePageTests to LandingPageTests
    Changes to: LandingPageTests.cs
    
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

---
+++ UnitTests/Views/Home/LandingPageTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+using Game;
+using Game.Views;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+
+namespace UnitTests.Views
+{
+    [TestFixture]
+    public class LandingPageTests
+    {
+        App app;
+        LandingPage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new LandingPage();
+        }
+
+        [TearDown]
+        public void TearDown() => Application.Current = null;
+
+        [Test]
+        public void HomePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void HomePage_GameButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+            // Act
+            page.EnterButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+    }
+}
