53c65e8334cd4d24426038a94ef8e2dfb1369a94
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-10 20:30:46 -0800

    Adds slimmed down version of rpgawesome and hexcode mapping class IconFont.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

---
+++ Game/Game/Common/Styles/IconFont.cs
@@ -0,0 +1,67 @@
+namespace Game.Styles
+{
+    static class IconFont
+    {
+        public const string Acid                = "\ue900";
+        public const string Acorn               = "\ue901";
+        public const string AllForOne           = "\ue902";
+        public const string Anvil               = "\ue903";
+        public const string Archer              = "\ue904";
+        public const string ArrowCluster        = "\ue905";
+        public const string ArrowFlights        = "\ue906";
+        public const string Arson               = "\ue907";
+        public const string Aura                = "\ue908";
+        public const string AxeSwing            = "\ue909";
+        public const string Axe                 = "\ue90a";
+        public const string BatteredAxe         = "\ue90b";
+        public const string Beer                = "\ue90c";
+        public const string BombExplosion       = "\ue90d";
+        public const string Book                = "\ue90e";
+        public const string BowieKnife          = "\ue90f";
+        public const string Broadsword          = "\ue910";
+        public const string BrokenBone          = "\ue911";
+        public const string BurningMeteor       = "\ue912";
+        public const string Cancel              = "\ue913";
+        public const string Carrot              = "\ue914";
+        public const string CastleEmblem        = "\ue915";
+        public const string Cheese              = "\ue916";
+        public const string CoffeeMug           = "\ue917";
+        public const string CorkedTube          = "\ue918";
+        public const string CrabClaw            = "\ue919";
+        public const string CrackedHelm         = "\ue91a";
+        public const string Crossbow            = "\ue91b";
+        public const string CrossedAxes         = "\ue91c";
+        public const string Cycle               = "\ue91d";
+        public const string DoubleTeam          = "\ue91e";
+        public const string DrippingKnife       = "\ue91f";
+        public const string DrippingSword       = "\ue920";
+        public const string Fish                = "\ue921";
+        public const string Forging             = "\ue922";
+        public const string GearHammer          = "\ue923";
+        public const string HammerDrop          = "\ue924";
+        public const string HeartBottle         = "\ue925";
+        public const string Helmet              = "\ue926";
+        public const string Honeycomb           = "\ue927";
+        public const string KeyBasic            = "\ue928";
+        public const string LightningSword      = "\ue929";
+        public const string Load                = "\ue92a";
+        public const string MoonSun             = "\ue92b";
+        public const string PlayerDespair       = "\ue92c";
+        public const string PlayerDodge         = "\ue92d";
+        public const string PlayerKing          = "\ue92e";
+        public const string PlayerLift          = "\ue92f";
+        public const string PlayerPain          = "\ue930";
+        public const string PlayerPyromaniac    = "\ue931";
+        public const string PlayerShot          = "\ue932";
+        public const string PlayerTeleport      = "\ue933";
+        public const string PlayerThunderStruck = "\ue934";
+        public const string Player              = "\ue935";
+        public const string PoisonCloud         = "\ue936";
+        public const string RoastChicken        = "\ue937";
+        public const string Save                = "\ue938";
+        public const string SpeechBubble        = "\ue939";
+        public const string SpeechBubbles       = "\ue93a";
+        public const string Torch               = "\ue93b";
+        public const string Trophy              = "\ue93c";
+    }
+}
