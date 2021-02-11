c71c85445272d953ca63315d8a27bf8adcb90c06
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-10 20:29:44 -0800

    Remove `ExportFont` assembly declarations from AssemblyInfo.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/AssemblyInfo.cs
+++ Game/Game/AssemblyInfo.cs
@@ -1,7 +1,3 @@
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: ExportFont("gamefont.tff", Alias = "8bit")]
-[assembly: ExportFont("gamefont_bold.tff", Alias = "8bit_bold")]
-[assembly: ExportFont("gamefont_small.tff", Alias = "8bit_small")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
