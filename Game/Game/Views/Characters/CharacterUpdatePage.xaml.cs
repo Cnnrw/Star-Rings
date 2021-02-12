fb4237c89bae8df285ed1081ca8ec2d89ce81557
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-12 13:54:19 -0800

    Adds UpdatePageBindingContext to CharacterUpdatePage.xaml.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel;
 
+using Game.GameRules;
 using Game.Models;
 using Game.ViewModels;
 
@@ -16,41 +17,158 @@ namespace Game.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterUpdatePage : ContentPage
     {
-        // The Character to create
-        private readonly GenericViewModel<CharacterModel> ViewModel = new GenericViewModel<CharacterModel>();
+        // The Character to update
+        private readonly GenericViewModel<CharacterModel> _viewModel;
 
         // Empty Constructor for UTs
         public CharacterUpdatePage(bool unitTest) { }
 
         /// <summary>
-        /// Constructor that takes and existing data item
+        ///     Constructor that takes and existing data item
         /// </summary>
         public CharacterUpdatePage(GenericViewModel<CharacterModel> data)
         {
             InitializeComponent();
 
-            BindingContext = this.ViewModel = data;
+            BindingContext = _viewModel = data;
 
-            JobPicker.SelectedItem = ViewModel.Data.Job.ToString();
+            LoadLevelPickerValues();
 
-            this.ViewModel.Title = "Update " + data.Title;
+            UpdatePageBindingContext();
         }
 
         /// <summary>
-        ///     Changes Level attribute of a Character
+        ///     Redo the Binding to cause a refresh
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OnLevelStepperChanged(object sender, ValueChangedEventArgs e) =>
-            LevelValueLabel.Text = $"{e.NewValue}";
+        /// <returns></returns>
+        private void UpdatePageBindingContext()
+        {
+            // Temp store off the level
+            var data = _viewModel.Data;
+
+            // Clear the Binding and reset it
+            BindingContext = null;
+            _viewModel.Data = data;
+            _viewModel.Title = data.Name;
+
+            BindingContext = _viewModel;
+
+            // this resets the Picker to the Character's level
+            LevelPicker.SelectedIndex = _viewModel.Data.Level - 1;
+
+            JobPicker.SelectedItem = _viewModel.Data.Job.ToString();
+        }
+
+        #region DataCRUDi
 
         /// <summary>
-        ///     Changes Max Health attribute of a Character
+        ///     Save by calling for Update
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void OnMaxHealthStepperChanged(object sender, ValueChangedEventArgs e) =>
-            MaxHealthValueLabel.Text = $"{e.NewValue}";
+        private async void Save_Clicked(object sender, EventArgs e)
+        {
+            if (_viewModel.Data.Name.Length == 0)
+            {
+                await DisplayAlert("Hold up!", "Please give your character a name", "OK");
+            }
+            else
+            {
+                MessagingCenter.Send(this, "Update", _viewModel.Data);
+                await Navigation.PopModalAsync();
+            }
+        }
+
+        /// <summary>
+        ///     Cancel the Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Cancel_Clicked(object sender, EventArgs e) => await Navigation.PopModalAsync();
+
+        // /// <summary>
+        // ///     Randomizes the Character values and items
+        // /// </summary>
+        // /// <param name="sender"></param>
+        // /// <param name="e"></param>
+        // private void RandomButton_Clicked(object sender, EventArgs e)
+        // {
+        //     _viewModel.Data.Update(RandomPlayerHelper.GetRandomCharacter(20));
+        //     UpdatePageBindingContext();
+        // }
+
+        #endregion
+
+        #region Picker
+
+        /// <summary>
+        ///     Propagates the values for the character level picker
+        /// </summary>
+        private void LoadLevelPickerValues()
+        {
+            for (var i = 1; i <= LevelTableHelper.MaxLevel; i++)
+            {
+                LevelPicker.Items.Add(i.ToString());
+            }
+
+            LevelPicker.SelectedIndex = -1;
+        }
+
+        /// <summary>
+        ///     Updates character level based on value selected from picker
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void LevelPicker_Changed(object sender, EventArgs args)
+        {
+            // If the Picker is not set, then set it
+            if (LevelPicker.SelectedIndex == -1)
+            {
+                LevelPicker.SelectedIndex = _viewModel.Data.Level - 1;
+                return;
+            }
+
+            var result = LevelPicker.SelectedIndex + 1;
+
+            // If the level hasn't changed, don't roll for health
+            if (result == _viewModel.Data.Level)
+            {
+                return;
+            }
+
+            // Change character level
+            _viewModel.Data.Level = result;
+
+            // Roll for new HP
+            _viewModel.Data.MaxHealth = RandomPlayerHelper.GetHealth(_viewModel.Data.Level);
+
+            UpdateHealthValue();
+        }
+
+        /// <summary>
+        ///     Updates character max health based on new level
+        /// </summary>
+        private void UpdateHealthValue() => MaxHealthValue.Text = _viewModel.Data.MaxHealth.ToString();
+
+        #endregion
+
+        #region Steppers
+
+        // /// <summary>
+        // ///     Changes Level attribute of a Character
+        // /// </summary>
+        // /// <param name="sender"></param>
+        // /// <param name="e"></param>
+        // private void OnLevelStepperChanged(object sender, ValueChangedEventArgs e) =>
+        //     LevelValueLabel.Text = $"{e.NewValue}";
+
+        // /// <summary>
+        // ///     Changes Max Health attribute of a Character
+        // /// </summary>
+        // /// <param name="sender"></param>
+        // /// <param name="e"></param>
+        // private void OnMaxHealthStepperChanged(object sender, ValueChangedEventArgs e) =>
+        //     MaxHealthValueLabel.Text = $"{e.NewValue}";
 
         /// <summary>
         ///     Changes Attack attribute of a Character
@@ -76,29 +194,6 @@ namespace Game.Views
         private void OnSpeedStepperChanged(object sender, ValueChangedEventArgs e) =>
             SpeedValueLabel.Text = $"{e.NewValue}";
 
-        /// <summary>
-        ///     Save by calling for Update
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void Save_Clicked(object sender, EventArgs e)
-        {
-            if (ViewModel.Data.Name.Length == 0)
-            {
-                await DisplayAlert("Hold up!", "Please give your character a name", "OK");
-            }
-            else
-            {
-                MessagingCenter.Send(this, "Update", ViewModel.Data);
-                await Navigation.PopModalAsync();
-            }
-        }
-
-        /// <summary>
-        ///     Cancel the Update
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void Cancel_Clicked(object sender, EventArgs e) => await Navigation.PopModalAsync();
+        #endregion
     }
 }
