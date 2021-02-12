f84cb4fd6c3c27292b0f4674184c998f53638d0b
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-12 11:57:27 -0800

    Code cleanup: GenericViewModel.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/ViewModels/GenericViewModel.cs
+++ Game/Game/ViewModels/GenericViewModel.cs
@@ -2,34 +2,19 @@
 
 namespace Game.ViewModels
 {
-    public class GenericViewModel<T> : BaseViewModel<DefaultModel> where T: class
+    public class GenericViewModel<T> : BaseViewModel<DefaultModel> where T : class
     {
-        /// <summary>
-        /// The Item Model
-        /// </summary>
-        T bindingData { get; set; }
-
-        public T Data
-        {
-            get { return bindingData; }
-            set {
-                var data = bindingData;
-                SetProperty(ref data, value);
-                bindingData = data;
-            }
-        }
 
         /// <summary>
-        /// Constructor takes an existing item and sets
-        /// The Title for the page to match the text of data
-        /// The Data to be the passed in data
+        ///     Constructor takes an existing item and sets
+        ///     the Title for the page to match the text of data
         /// </summary>
         /// <param name="data"></param>
         public GenericViewModel(T data)
         {
             if (data != null)
             {
-                Title = (data as BaseModel<T>).Name;
+                Title = (data as BaseModel<T>)?.Name;
             }
             Data = data;
         }
@@ -37,6 +22,22 @@ namespace Game.ViewModels
         // Generic Constructor
         public GenericViewModel()
         {
+        }
+
+        /// <summary>
+        ///     The Item Model
+        /// </summary>
+        T bindingData { get; set; }
+
+        public T Data
+        {
+            get => bindingData;
+            set
+            {
+                var data = bindingData;
+                SetProperty(ref data, value);
+                bindingData = data;
+            }
         }
     }
 }
