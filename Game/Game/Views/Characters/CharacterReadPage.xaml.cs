4c6ae83f036b0e34e08a5d1332231c3ddb2171d7
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-12 13:47:49 -0800

    Code cleanup: CharacterReadPage.xaml.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Views/Characters/CharacterReadPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterReadPage.xaml.cs
@@ -1,58 +1,62 @@
 using System;
 using System.ComponentModel;
+
+using Game.Models;
+using Game.ViewModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
-using Game.ViewModels;
-using Game.Models;
 
 namespace Game.Views
 {
     /// <summary>
-    /// The Read Page
+    ///     Character Read Page code-behind
     /// </summary>
     [DesignTimeVisible(false)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CharacterReadPage : ContentPage
     {
         // View Model for Item
-        public readonly GenericViewModel<CharacterModel> ViewModel;
+        private readonly GenericViewModel<CharacterModel> _viewModel;
 
         // Empty Constructor for UTs
         public CharacterReadPage(bool UnitTest) { }
 
         /// <summary>
-        /// Constructor called with a view model
-        /// This is the primary way to open the page
-        /// The viewModel is the data that should be displayed
+        ///     Constructor called with a view model
+        ///     This is the primary way to open the page
+        ///     The ViewModel is the data that should be displayed
         /// </summary>
-        /// <param name="viewModel"></param>
+        /// <param name="data"></param>
         public CharacterReadPage(GenericViewModel<CharacterModel> data)
         {
             InitializeComponent();
 
-            BindingContext = this.ViewModel = data;
+            _viewModel = data;
+            _viewModel.Title = data.Data.Name;
+
+            BindingContext = _viewModel;
         }
 
         /// <summary>
-        /// Save calls to Update
+        ///     Save calls to Update
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void Update_Clicked(object sender, EventArgs e)
+        private async void Update_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new CharacterUpdatePage(ViewModel)));
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterUpdatePage(_viewModel)));
             await Navigation.PopAsync();
         }
 
         /// <summary>
-        /// Calls for Delete
+        ///     Calls for Delete
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void Delete_Clicked(object sender, EventArgs e)
+        private async void Delete_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new NavigationPage(new CharacterDeletePage(ViewModel)));
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterDeletePage(_viewModel)));
             await Navigation.PopAsync();
         }
     }
