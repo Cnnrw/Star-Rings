148f5914dd8df1d834d2a42a55e7622ff8ca33a5
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-13 15:39:11 -0800

    Refactors conflicting property names
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterUpdatePage.xaml.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 using Game.GameRules;
 using Game.Models;
@@ -62,6 +64,7 @@ namespace Game.Views
 
         #endregion
 
+
         #region Steppers
 
         private void OnSpeedStepperValueChanged(object sender, ValueChangedEventArgs e) =>
@@ -104,6 +107,7 @@ namespace Game.Views
         #endregion
 
         #region UniqueItems
+
         /// <summary>
         /// Show the Items the Character has
         /// </summary>
@@ -123,7 +127,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="args"></param>
-        public void OnPopupItemSelected(object sender, SelectedItemChangedEventArgs args)
+        private void OnPopupItemSelected(object sender, SelectedItemChangedEventArgs args)
         {
             ItemModel data = args.SelectedItem as ItemModel;
             if (data == null)
@@ -131,7 +135,7 @@ namespace Game.Views
                 return;
             }
 
-            ViewModel.Data.UniqueItem = data.Id;
+            _viewModel.Data.UniqueItem = data.Id;
 
             AddItemsToDisplay();
 
@@ -141,22 +145,25 @@ namespace Game.Views
         /// <summary>
         /// Look up the Item to Display
         /// </summary>
-        /// <param name="location"></param>
         /// <returns></returns>
         public StackLayout GetItemToDisplay()
         {
-            var data = ViewModel.Data.GetItem(ViewModel.Data.UniqueItem);
+            var data = _viewModel.Data.GetItem(_viewModel.Data.UniqueItem);
             if (data == null)
             {
                 // Show the Default Icon for the Location
-                data = new ItemModel { Location = ItemLocationEnum.Unknown, ImageURI = "icon_cancel.png", Name = "Click to Add" };
+                data = new ItemModel
+                {
+                    Location = ItemLocationEnum.Unknown,
+                    ImageURI = "icon_cancel.png",
+                    Name = "Click to Add"
+                };
             }
 
             // Hookup the Image Button to show the Item picture
             var ItemButton = new ImageButton
             {
-                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
-                Source = data.ImageURI
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"], Source = data.ImageURI
             };
 
             // Add a event to the user can click the item and see more
@@ -177,10 +184,7 @@ namespace Game.Views
                 Padding = 3,
                 Style = (Style)Application.Current.Resources["ItemImageBox"],
                 HorizontalOptions = LayoutOptions.Center,
-                Children = {
-                    ItemButton,
-                    ItemLabel
-                },
+                Children = {ItemButton, ItemLabel},
             };
 
             return ItemStack;
@@ -191,24 +195,21 @@ namespace Game.Views
         /// </summary>
         /// <param name="data"></param>
         /// <returns></returns>
-        public bool ShowPopup(ItemModel data)
+        private bool ShowPopup(ItemModel data)
         {
             PopupItemSelector.IsVisible = true;
 
             // Make a fake item for None
             var NoneItem = new ItemModel
             {
-                Id = null, // will use null to clear the item
+                Id = null,     // will use null to clear the item
                 Guid = "None", // how to find this item amoung all of them
                 ImageURI = "icon_cancel.png",
                 Name = "None",
                 Description = "None"
             };
 
-            List<ItemModel> itemList = new List<ItemModel>
-            {
-                NoneItem
-            };
+            List<ItemModel> itemList = new List<ItemModel> {NoneItem};
 
             // Add the rest of the items to the list
             itemList.AddRange(ItemIndexViewModel.Instance.Dataset);
@@ -218,7 +219,6 @@ namespace Game.Views
             return true;
         }
 
-
         /// <summary>
         /// When the user clicks the close in the Popup
         /// hide the view
@@ -226,18 +226,12 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public void ClosePopup_Clicked(object sender, EventArgs e)
-        {
-            ClosePopup();
-        }
+        private void ClosePopup_Clicked(object sender, EventArgs e) => ClosePopup();
 
         /// <summary>
         /// Close the popup
         /// </summary>
-        public void ClosePopup()
-        {
-            PopupItemSelector.IsVisible = false;
-        }
+        private void ClosePopup() => PopupItemSelector.IsVisible = false;
 
         #endregion UniqueItems
     }
