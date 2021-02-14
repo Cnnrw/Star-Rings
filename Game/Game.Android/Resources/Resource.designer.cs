5470f2c27a4bab68efdf8fa577950bbb21eefa32
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-14 09:48:54 -0800

    Update to Resource.designer.cs in Game.Android.Resources
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game.Android/Resources/Resource.designer.cs
+++ Game/Game.Android/Resources/Resource.designer.cs
@@ -11261,13 +11261,19 @@ namespace Game.Droid
 			public const int StartButton_background = 2131165191;
 			
 			// aapt resource value: 0x7F070098
-			public const int test_custom_background = 2131165336;
+			public const int start_button_normal = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int tooltip_frame_dark = 2131165337;
+			public const int start_button_pressed = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int tooltip_frame_light = 2131165338;
+			public const int test_custom_background = 2131165338;
+			
+			// aapt resource value: 0x7F07009B
+			public const int tooltip_frame_dark = 2131165339;
+			
+			// aapt resource value: 0x7F07009C
+			public const int tooltip_frame_light = 2131165340;
 			
 			static Drawable()
 			{
