ee19c4496c448d2b0aa38f728ffc816ca8e263e0
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-13 22:34:12 -0800

    Adds custom renderer to Game.Android for transparent navbar
    Changes to: CustomNavigationPageRenderer.cs, Game.Android.csproj
    
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

---
+++ Game/Game.Android/Renderers/CustomNavigationPageRenderer.cs
@@ -0,0 +1,50 @@
+using Android.Content;
+using Android.Widget;
+
+using Game.Controls;
+using Game.Droid.Renderers;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+using AView = Android.Views.View;
+
+[assembly: ExportRenderer(typeof(CustomNavigationPage), typeof(CustomNavigationPageRenderer))]
+
+namespace Game.Droid.Renderers
+{
+    public class CustomNavigationPageRenderer : NavigationPageRenderer
+    {
+        public CustomNavigationPageRenderer(Context context) : base(context)
+        {
+        }
+
+        IPageController PageController => Element as IPageController;
+        CustomNavigationPage CustomNavigationPage => Element as CustomNavigationPage;
+
+        protected override void OnLayout(bool changed, int l, int t, int r, int b)
+        {
+            CustomNavigationPage.IgnoreLayoutChange = true;
+            base.OnLayout(changed, l, t, r, b);
+            CustomNavigationPage.IgnoreLayoutChange = false;
+
+            var containerHeight = b - t;
+
+            PageController.ContainerArea =
+                new Rectangle(0, 0, Context.FromPixels(r - l), Context.FromPixels(containerHeight));
+
+            for (var i = 0; i < ChildCount; i++)
+            {
+                AView child = GetChildAt(i);
+
+                if (child is Toolbar)
+                {
+                    continue;
+                }
+
+                child?.Layout(0, 0, r, b);
+            }
+        }
+    }
+}
