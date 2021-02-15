cf36e4a85762584f29ff9c52a4d903bb73e9fd14
Tate <tatebrasel@gmail.com>
2021-02-15 12:51:57 -0800

    Added equipped item icons to CharacterReadPage. Changes in CharacterReadPage.xaml, CharacterReadPage.xaml.cs

--- Game/Game/Views/Characters/CharacterReadPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterReadPage.xaml.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 using Game.Models;
+using Game.Models.Enums;
 using Game.ViewModels;
 
 using Xamarin.Forms;
@@ -36,6 +39,8 @@ namespace Game.Views
             _viewModel.Title = data.Data.Name;
 
             BindingContext = _viewModel;
+
+            AddItemsToDisplay();
         }
 
         /// <summary>
@@ -59,5 +64,109 @@ namespace Game.Views
             await Navigation.PushModalAsync(new NavigationPage(new CharacterDeletePage(_viewModel)));
             await Navigation.PopAsync();
         }
+
+        #region EquippedItems
+
+        /// <summary>
+        /// Show the Items the Character has
+        /// </summary>
+        private void AddItemsToDisplay()
+        {
+            var FlexList = ItemBox.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                ItemBox.Children.Remove(data);
+            }
+
+            // Add an item display box for each Item Location 
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Head));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Necklace));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.PrimaryHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.OffHand));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.LeftFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.RightFinger));
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Feet));
+        }
+
+        /// <summary>
+        /// Look up the Item to Display
+        /// </summary>
+        /// <returns></returns>
+        private StackLayout GetItemToDisplay(ItemLocationEnum itemLocation)
+        {
+            ItemModel data = null;
+
+            // Get the current Item in this ItemLocation
+            switch (itemLocation)
+            {
+                case ItemLocationEnum.Head:
+                    data = _viewModel.Data.GetItem(_viewModel.Data.Head);
+                    break;
+                case ItemLocationEnum.Necklace:
+                    data = _viewModel.Data.GetItem(_viewModel.Data.Necklace);
+                    break;
+                case ItemLocationEnum.PrimaryHand:
+                    data = _viewModel.Data.GetItem(_viewModel.Data.PrimaryHand);
+                    break;
+                case ItemLocationEnum.OffHand:
+                    data = _viewModel.Data.GetItem(_viewModel.Data.OffHand);
+                    break;
+                case ItemLocationEnum.LeftFinger:
+                    data = _viewModel.Data.GetItem(_viewModel.Data.LeftFinger);
+                    break;
+                case ItemLocationEnum.RightFinger:
+                    data = _viewModel.Data.GetItem(_viewModel.Data.RightFinger);
+                    break;
+                case ItemLocationEnum.Feet:
+                    data = _viewModel.Data.GetItem(_viewModel.Data.Feet);
+                    break;
+                default:
+                    break;
+            }
+
+            // If there's no Item currently in the slot, show a blank Item
+            data = data ??
+                new ItemModel
+                {
+                    Location = ItemLocationEnum.Unknown,
+                    ImageURI = "icon_cancel.png",
+                    Name = "No item"
+                };
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Source = data.ImageURI,
+                Style = Application.Current.Resources.TryGetValue("ImageMediumStyle", out object buttonStyle)
+                            ? (Style)buttonStyle
+                            : null
+            };
+
+            // Add the Display Text for the item
+            var ItemLabel = new Label
+            {
+                Text = data.Name,
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Style = Application.Current.Resources.TryGetValue("ValueStyleMicro", out object labelStyle)
+                            ? (Style)labelStyle
+                            : null
+            };
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                Style = Application.Current.Resources.TryGetValue("ItemImageBox", out object stackStyle)
+                            ? (Style)stackStyle
+                            : null,
+                HorizontalOptions = LayoutOptions.Center,
+                Children = { ItemButton, ItemLabel }
+            };
+
+            return ItemStack;
+        }
+
+        #endregion EquippedItems
     }
 }
