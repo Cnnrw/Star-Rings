0227d4a1c7d9ceeb0bb5053f16035352353b2d55
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-02-15 11:58:05 -0800

    Adds logo to landingpage, alters global styles
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game.Android/Resources/Resource.designer.cs
+++ Game/Game.Android/Resources/Resource.designer.cs
@@ -11275,23 +11275,26 @@ namespace Game.Droid
 			// aapt resource value: 0x7F070007
 			public const int StartButton_background = 2131165191;
 			
-			// aapt resource value: 0x7F07009D
-			public const int start_button_normal = 2131165341;
-			
 			// aapt resource value: 0x7F07009E
-			public const int start_button_pressed = 2131165342;
+			public const int start_button_normal = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int test_custom_background = 2131165343;
+			public const int start_button_pressed = 2131165343;
+			
+			// aapt resource value: 0x7F07009D
+			public const int star_rings_logo = 2131165341;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int tooltip_frame_dark = 2131165344;
+			public const int test_custom_background = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int tooltip_frame_light = 2131165345;
+			public const int tooltip_frame_dark = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int wookie_icon = 2131165346;
+			public const int tooltip_frame_light = 2131165346;
+			
+			// aapt resource value: 0x7F0700A3
+			public const int wookie_icon = 2131165347;
 			
 			static Drawable()
 			{
