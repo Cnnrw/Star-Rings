5bdf1ee25fc1dc820cb5cae02faaae79ffed9776
Tate <tatebrasel@gmail.com>
2021-02-21 22:00:23 -0800

    Fixed DefaultData_Valid_LoadData_Monster_Should_Pass in DefaultDataTests.cs

--- UnitTests/GameRules/DefaultDataTests.cs
+++ UnitTests/GameRules/DefaultDataTests.cs
@@ -49,7 +49,7 @@ namespace UnitTests.GameRules
             // Reset
 
             // Assert
-            Assert.AreEqual(7, result.Count());
+            Assert.AreEqual(3, result.Count());
         }
 
 
