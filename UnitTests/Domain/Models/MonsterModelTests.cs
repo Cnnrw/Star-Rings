d2273e8e645f4ed913182defa22532c45838dae5
Jack Witt <jwitt1452@gmail.com>
2021-02-22 14:34:36 -0800

    Updated MonsterModel_FormatOutput_Default_Should_Pass to use our default name

--- UnitTests/Domain/Models/MonsterModelTests.cs
+++ UnitTests/Domain/Models/MonsterModelTests.cs
@@ -154,7 +154,7 @@ namespace UnitTests.Models
             // Reset
 
             // Assert
-            Assert.AreEqual(true, result.Contains("Goblin"));
+            Assert.AreEqual(true, result.Contains("Orc"));
         }
 
         [Test]
