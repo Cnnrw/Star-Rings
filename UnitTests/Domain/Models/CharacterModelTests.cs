9d1cf4f66cdea34242a8411b774d63b571371216
Tate <tatebrasel@gmail.com>
2021-02-21 22:53:38 -0800

    Fixed CharacterModel_GetDefense_Wookie_Should_Pass in CharatcerModelTests.cs

--- UnitTests/Domain/Models/CharacterModelTests.cs
+++ UnitTests/Domain/Models/CharacterModelTests.cs
@@ -298,7 +298,7 @@ namespace UnitTests.Models
             // Reset
 
             // Assert
-            Assert.AreEqual(3, result);
+            Assert.AreEqual(1, result);
         }
 
         [Test]
