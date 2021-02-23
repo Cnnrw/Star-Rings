95d912d9865863fdf1ab64c0cef32018dc6c5979
Tate <tatebrasel@gmail.com>
2021-02-22 21:48:31 -0800

    Added BattleLocationEnumHelperTests.cs to UT Project

---
+++ UnitTests/Helpers/BattleLocationEnumHelperTests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+
+using Game.Enums;
+
+using NUnit.Framework;
+
+namespace UnitTests.Helpers
+{
+    [TestFixture]
+    class BattleLocationEnumHelperTests
+    {
+
+    }
+}
