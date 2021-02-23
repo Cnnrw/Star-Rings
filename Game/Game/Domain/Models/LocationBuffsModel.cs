9bcd29805cb686b7053b88e50e8f46584d62c5fe
Tate <tatebrasel@gmail.com>
2021-02-22 23:33:34 -0800

    Removed LocationBuffsModel from Project

--- Game/Game/Domain/Models/LocationBuffsModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Game.Models
-{
-    public class LocationBuffsModel
-    {
-        public int AttackBuffValue { get; set; }
-        public int DefenseBuffValue { get; set; }
-        public int SpeedBuffValue { get; set; }
-    }
-}
