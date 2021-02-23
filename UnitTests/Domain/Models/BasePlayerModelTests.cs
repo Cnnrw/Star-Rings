e27c2e8de1e7edc95c460531909428ad6f74d90a
Tate <tatebrasel@gmail.com>
2021-02-22 21:06:50 -0800

    Fixed BasePlayerModel_Constructor_Default_Should_Pass in BasePlayerModelTests.cs

--- UnitTests/Domain/Models/BasePlayerModelTests.cs
+++ UnitTests/Domain/Models/BasePlayerModelTests.cs
@@ -34,7 +34,7 @@ namespace UnitTests.Models
             // Reset
 
             // Assert
-            Assert.AreEqual("This is an Item", result.Name);
+            Assert.AreEqual("Player", result.Name);
         }
 
         [Test]
