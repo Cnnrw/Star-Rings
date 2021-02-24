d7eaca781aa2ad9c7e7941a47e0f5db1a3ffb1b8
Tate <tatebrasel@gmail.com>
2021-02-23 18:52:57 -0800

    Added test CharacterModel_GetSpeed_ProtocolDroid_Should_Pass to CharacterModelTests.cs

--- UnitTests/Domain/Models/CharacterModelTests.cs
+++ UnitTests/Domain/Models/CharacterModelTests.cs
@@ -422,6 +422,21 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void CharacterModel_GetSpeed_ProtocolDroid_Should_Pass()
+        {
+            // ArDefense
+            var data = new CharacterModel { Job = CharacterJobEnum.ProtocolDroid };
+
+            // Act
+            var result = data.GetSpeed();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
         public void CharacterModel_GetAttack_Default_Should_Pass()
         {
             // ArDefense
