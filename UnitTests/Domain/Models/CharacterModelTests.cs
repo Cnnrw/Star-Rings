ff00eb21f45f8fc059dbd57c0103ecce7634f52b
Tate <tatebrasel@gmail.com>
2021-02-23 18:47:39 -0800

    Added test CharacterModel_GetSpeed_Princess_Should_Pass to CharacterModelTests.cs

--- UnitTests/Domain/Models/CharacterModelTests.cs
+++ UnitTests/Domain/Models/CharacterModelTests.cs
@@ -362,6 +362,21 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void CharacterModel_GetSpeed_Princess_Should_Pass()
+        {
+            // ArDefense
+            var data = new CharacterModel { Job = CharacterJobEnum.Princess };
+
+            // Act
+            var result = data.GetSpeed();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
         public void CharacterModel_GetSpeed_Wookie_Should_Pass()
         {
             // ArDefense
