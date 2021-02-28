524da1f4af420a10f0ad6e79d58c34337a748a70
Tate <tatebrasel@gmail.com>
2021-02-27 23:28:57 -0800

    Made AddCharacter_Clicked public

--- Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -53,7 +53,7 @@ namespace Game.Views
     /// </summary>
     /// <param name="sender"></param>
     /// <param name="e"></param>
-    private async void AddCharacter_Clicked(object sender, EventArgs e) =>
+    public async void AddCharacter_Clicked(object sender, EventArgs e) =>
       await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage()));
 
     /// <summary>
