016b5e149c11325a66d5feb4f21ddd94b11be0e3
Tate <tatebrasel@gmail.com>
2021-02-27 21:30:40 -0800

    Added navigation to AutoBattlePage from BattleHomePage

--- Game/Game/Views/Battle/BattleHomePage.xaml.cs
+++ Game/Game/Views/Battle/BattleHomePage.xaml.cs
@@ -24,5 +24,15 @@ namespace Game.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        /// <summary>
+        /// Navigate to the auto-battle page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void AutoBattle_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AutoBattlePage());
+        }
+    }
 }
