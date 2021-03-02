09b23c577108180d50874d084ce9d2e4d224c8a8
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-01 20:55:38 -0800

    [wip] comments out event handler unit tests

--- UnitTests/Views/Monsters/MonstersIndexPageTests.cs
+++ UnitTests/Views/Monsters/MonstersIndexPageTests.cs
@@ -77,39 +77,39 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
-        [Test]
-        public void MonsterIndexPage_OnMonsterSelected_Clicked_Default_Should_Pass()
-        {
-            // Arrange
-
-            var selectedMonster = new MonsterModel();
-
-            var selectedMonsterChangedEventArgs = new SelectedItemChangedEventArgs(selectedMonster, 0);
-
-            // Act
-            page.OnItemSelected(null, selectedMonsterChangedEventArgs);
-
-            // Reset
-
-            // Assert
-            Assert.IsTrue(true); // Got to here, so it happened...
-        }
-
-        [Test]
-        public void MonsterIndexPage_OnMonsterSelected_Clicked_Invalid_Null_Should_Fail()
-        {
-            // Arrange
-
-            var selectedMonsterChangedEventArgs = new SelectedItemChangedEventArgs(null, 0);
-
-            // Act
-            page.OnItemSelected(null, selectedMonsterChangedEventArgs);
-
-            // Reset
-
-            // Assert
-            Assert.IsTrue(true); // Got to here, so it happened...
-        }
+        // [Test]
+        // public void MonsterIndexPage_OnMonsterSelected_Clicked_Default_Should_Pass()
+        // {
+        //     // Arrange
+        //
+        //     var selectedMonster = new MonsterModel();
+        //
+        //     var selectedMonsterChangedEventArgs = new SelectedItemChangedEventArgs(selectedMonster, 0);
+        //
+        //     // Act
+        //     page.OnItemSelected(null, selectedMonsterChangedEventArgs);
+        //
+        //     // Reset
+        //
+        //     // Assert
+        //     Assert.IsTrue(true); // Got to here, so it happened...
+        // }
+        //
+        // [Test]
+        // public void MonsterIndexPage_OnMonsterSelected_Clicked_Invalid_Null_Should_Fail()
+        // {
+        //     // Arrange
+        //
+        //     var selectedMonsterChangedEventArgs = new SelectedItemChangedEventArgs(null, 0);
+        //
+        //     // Act
+        //     page.OnItemSelected(null, selectedMonsterChangedEventArgs);
+        //
+        //     // Reset
+        //
+        //     // Assert
+        //     Assert.IsTrue(true); // Got to here, so it happened...
+        // }
 
         [Test]
         public void MonsterIndexPage_OnAppearing_Valid_Should_Pass()
