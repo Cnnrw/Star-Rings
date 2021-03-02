bdde781f9bf932b5cce1d7ffda3bb13ed9dbc36a
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-01 21:29:38 -0800

    Updates BattleHomePage styling

--- Game/Game/Views/Battle/BattleHomePage.xaml.cs
+++ Game/Game/Views/Battle/BattleHomePage.xaml.cs
@@ -1,58 +1,42 @@
 using System;
 
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-using Game.Engine.EngineKoenig;
-using Game.Models;
-using Game.ViewModels;
-using Game.Engine.EngineInterfaces;
+using Game.Templates.Pages;
 
 namespace Game.Views
 {
 	/// <summary>
 	/// The Main Game Page
 	/// </summary>
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class BattleHomePage : ContentPage
+	public partial class BattleHomePage : ModalPage
 	{
-
 		/// <summary>
 		/// Constructor
 		/// </summary>
-		public BattleHomePage ()
-		{
+		public BattleHomePage () =>
 			InitializeComponent ();
-		}
 
         /// <summary>
         /// Navigate to the auto-battle page
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void AutoBattle_Clicked(object sender, EventArgs e)
-        {
+        public async void AutoBattle_Clicked(object sender, EventArgs e) =>
             await Navigation.PushAsync(new AutoBattlePage());
-        }
 
         /// <summary>
         /// Navigate to the pick characters page
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void PickCharacters_Clicked(object sender, EventArgs e)
-        {
+        public async void PickCharacters_Clicked(object sender, EventArgs e) =>
             await Navigation.PushAsync(new PickCharactersPage());
-        }
 
         /// <summary>
         /// Navigate to the new round page
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void PickItems_Clicked(object sender, EventArgs e)
-        {
+        public async void PickItems_Clicked(object sender, EventArgs e) =>
             await Navigation.PushAsync(new PickItemsPage());
-        }
     }
 }
