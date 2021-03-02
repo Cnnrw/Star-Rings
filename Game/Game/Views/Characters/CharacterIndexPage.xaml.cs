5a798e83d3fdc4be892464d43d0b4e11e56dd612
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-01 19:39:31 -0800

    Refactors CharacterIndexPage to use a CollectionView instead of ListView

--- Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 using Game.Models;
 using Game.Templates.Pages;
@@ -8,77 +9,74 @@ using Xamarin.Forms;
 
 namespace Game.Views
 {
-  /// <summary>
-  ///     Character Index Page
-  /// </summary>
-  public partial class CharacterIndexPage : ModalPage
-  {
-    private readonly CharacterIndexViewModel _viewModel = CharacterIndexViewModel.Instance;
-
-    // Empty Constructor for UTs
-    internal CharacterIndexPage(bool unitTest) { }
-
     /// <summary>
-    ///     Constructor for Index Page
-    ///     Get the CharacterIndexView Model
+    ///     Character Index Page
     /// </summary>
-    public CharacterIndexPage()
+    public partial class CharacterIndexPage : ModalPage
     {
-      InitializeComponent();
+        private readonly CharacterIndexViewModel _viewModel = CharacterIndexViewModel.Instance;
 
-      BindingContext = _viewModel;
+        // Empty Constructor for UTs
+        internal CharacterIndexPage(bool unitTest) { }
+
+        /// <summary>
+        ///     Constructor for Index Page
+        ///     Get the CharacterIndexView Model
+        /// </summary>
+        public CharacterIndexPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel;
+        }
+
+        /// <summary>
+        ///     The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void OnCharacterSelected(object sender, SelectionChangedEventArgs args)
+        {
+            if (!(args.CurrentSelection.FirstOrDefault() is CharacterModel data))
+                return;
+
+            // Open the Read Page
+            await Navigation.PushAsync(new CharacterReadPage(new GenericViewModel<CharacterModel>(data)));
+
+            // Manually deselect item.
+            CharacterList.SelectedItem = null;
+        }
+
+        /// <summary>
+        ///     Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void AddCharacter_Clicked(object sender, EventArgs e) =>
+            await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage()));
+
+        /// <summary>
+        ///     Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (_viewModel.Dataset.Count == 0)
+            {
+                _viewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (_viewModel.NeedsRefresh())
+            {
+                _viewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = _viewModel;
+        }
     }
-
-    /// <summary>
-    ///     The row selected from the list
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="args"></param>
-    public async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
-    {
-      var data = (CharacterModel)args.SelectedItem;
-      if (data == null)
-      {
-        return;
-      }
-
-      // Open the Read Page
-      await Navigation.PushAsync(new CharacterReadPage(new GenericViewModel<CharacterModel>(data)));
-
-      // Manually deselect item.
-      CharactersListView.SelectedItem = null;
-    }
-
-    /// <summary>
-    ///     Call to Add a new record
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    public async void AddCharacter_Clicked(object sender, EventArgs e) =>
-      await Navigation.PushModalAsync(new NavigationPage(new CharacterCreatePage()));
-
-    /// <summary>
-    ///     Refresh the list on page appearing
-    /// </summary>
-    protected override void OnAppearing()
-    {
-      base.OnAppearing();
-
-      BindingContext = null;
-
-      // If no data, then set it for needing refresh
-      if (_viewModel.Dataset.Count == 0)
-      {
-        _viewModel.SetNeedsRefresh(true);
-      }
-
-      // If the needs Refresh flag is set update it
-      if (_viewModel.NeedsRefresh())
-      {
-        _viewModel.LoadDatasetCommand.Execute(null);
-      }
-
-      BindingContext = _viewModel;
-    }
-  }
 }
