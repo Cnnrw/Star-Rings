94e3df3526f14b6239a36b9711a95dbaa1fa15c3
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-01 19:23:39 -0800

    Adds CharacterCell to Templates/Data

---
+++ Game/Game/Views/Templates/Data/CharacterCell.xaml.cs
@@ -0,0 +1,13 @@
+using Xamarin.Forms;
+
+namespace Game.Views.Templates.Data
+{
+    public partial class CharacterCell : Grid
+    {
+        public CharacterCell()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
