88430cff315c9dea110f83ee41ea61b6ea87ed66
Tate <tatebrasel@gmail.com>
2021-03-01 22:56:47 -0800

    Fixed test MonsterIndexViewModel_Message_WipeDataList_Valid_Should_Pass in MonsterIndexViewModelTets.cs

--- UnitTests/Domain/ViewModels/MonsterIndexViewModelTests.cs
+++ UnitTests/Domain/ViewModels/MonsterIndexViewModelTests.cs
@@ -253,7 +253,7 @@ namespace UnitTests.ViewModels
             // Reset
 
             // Assert
-            Assert.AreEqual(9, countAfter); // Count of 0 for the load was skipped
+            Assert.AreEqual(12, countAfter); // Count of 0 for the load was skipped
         }
     }
 }
