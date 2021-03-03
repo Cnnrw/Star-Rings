e2c5d2bae8c1441b544ec5942e13bb01133f428e
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-02 16:54:17 -0800

    Change namespace of global styles from Common.Styles to Game.Styles

--- Game/Game/Assets/Styles/DefaultTheme.xaml.cs
+++ Game/Game/Assets/Styles/DefaultTheme.xaml.cs
@@ -2,7 +2,7 @@ using Xamarin.Forms;
 
 [assembly: ExportFont("gamefont.ttf", Alias = "8bit")]
 [assembly: ExportFont("gamefont_bold.ttf", Alias = "8bit_bold")]
-namespace Common.Styles
+namespace Game.Styles
 {
     public partial class DefaultTheme : ResourceDictionary
     {
