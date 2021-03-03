e1183819acd8dd04743fb2fe06c9531c302986a9
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-02 16:48:01 -0800

    Remove unused rpgawesome font from project

--- Game/Game/Assets/Styles/DefaultTheme.xaml.cs
+++ Game/Game/Assets/Styles/DefaultTheme.xaml.cs
@@ -2,7 +2,6 @@ using Xamarin.Forms;
 
 [assembly: ExportFont("gamefont.ttf", Alias = "8bit")]
 [assembly: ExportFont("gamefont_bold.ttf", Alias = "8bit_bold")]
-[assembly: ExportFont("rpgawesome.ttf", Alias = "rpg")]
 namespace Common.Styles
 {
     public partial class DefaultTheme : ResourceDictionary
