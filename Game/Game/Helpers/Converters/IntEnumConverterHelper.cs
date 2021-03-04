ade2b0fb798b0f4ace0ed54c90b4dfc723cc36df
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-04 15:04:33 -0800

    Refactor IntEnumConverterHelper methods to use ternary return statements

--- Game/Game/Helpers/Converters/IntEnumConverterHelper.cs
+++ Game/Game/Helpers/Converters/IntEnumConverterHelper.cs
@@ -5,50 +5,42 @@ using Xamarin.Forms;
 
 namespace Game.Converters
 {
-    // This converter is used by the Pickers, to change from the picker value to the string value etc.
-    // This allows the convert in the picker to be data bound back and forth the model
-    // The picker requires this because the picker must be a string, but the enum is a value...
 
     /// <summary>
-    /// Converts from an Int to an Enum value
+    ///     Converts from an Int to an Enum value
+    ///     <remarks>
+    ///         This converter is used by the Pickers, to change from the picker value to the string value etc.
+    ///         This allows the convert in the picker to be data bound back and forth the model
+    ///         The picker requires this because the picker must be a string, but the enum is a value...
+    ///     </remarks>
     /// </summary>
     public class IntEnumConverter : IValueConverter
     {
         /// <summary>
-        /// Converts String to Int
+        ///     Converts a string to an int
         /// </summary>
         /// <param name="value"></param>
         /// <param name="targetType"></param>
         /// <param name="parameter"></param>
         /// <param name="culture"></param>
         /// <returns></returns>
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is Enum)
-            {
-                return (int)value;
-            }
-
-            return 0;
-        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is Enum
+                ? (int)value
+                : 0;
 
         /// <summary>
-        /// Converts from Int to String
+        ///     Converts from Int to String
         /// </summary>
         /// <param name="value"></param>
         /// <param name="targetType"></param>
         /// <param name="parameter"></param>
         /// <param name="culture"></param>
         /// <returns></returns>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is int)
-            {
-                var myReturn = Enum.ToObject(targetType, value);
-                return myReturn.ToString();
-            }
-
-            return 0;
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is int
+                ? (object)Enum.ToObject(targetType, value)
+                              .ToString()
+                : 0;
     }
 }
