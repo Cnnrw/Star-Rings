01a54dfc4f2b035d2da208a2db55e761750a9b50
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-04 15:05:07 -0800

    [cc] IntEnumConverterHelperTests.cs

--- UnitTests/Helpers/IntEnumConverterHelperTests.cs
+++ UnitTests/Helpers/IntEnumConverterHelperTests.cs
@@ -15,10 +15,10 @@ namespace UnitTests.Helpers
             var myConverter = new IntEnumConverter();
 
             var myObject = "Bogus";
-            var Result = myConverter.Convert(myObject, null, null, null);
-            var Expected = 0;
+            var result = myConverter.Convert(myObject, null, null, null);
+            var expected = 0;
 
-            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(expected, result, TestContext.CurrentContext.Test.Name);
         }
 
         [Test]
@@ -27,10 +27,10 @@ namespace UnitTests.Helpers
             var myConverter = new IntEnumConverter();
 
             var myObject = ItemLocationEnum.Feet;
-            var Result = myConverter.Convert(myObject, null, null, null);
-            var Expected = 40;
+            var result = myConverter.Convert(myObject, null, null, null);
+            var expected = 40;
 
-            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(expected, result, TestContext.CurrentContext.Test.Name);
         }
 
         [Test]
@@ -39,10 +39,10 @@ namespace UnitTests.Helpers
             var myConverter = new StringEnumConverter();
 
             var myObject = "Feet";
-            var Result = myConverter.Convert(myObject, typeof(ItemLocationEnum), null, null);
-            var Expected = ItemLocationEnum.Feet;
+            var result = myConverter.Convert(myObject, typeof(ItemLocationEnum), null, null);
+            var expected = ItemLocationEnum.Feet;
 
-            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(expected, result, TestContext.CurrentContext.Test.Name);
         }
 
         [Test]
@@ -51,10 +51,10 @@ namespace UnitTests.Helpers
             var myConverter = new StringEnumConverter();
 
             var myObject = ItemLocationEnum.Feet;
-            var Result = myConverter.Convert(myObject, null, null, null);
-            var Expected = 40;
+            var result = myConverter.Convert(myObject, null, null, null);
+            var expected = 40;
 
-            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(expected, result, TestContext.CurrentContext.Test.Name);
         }
 
         [Test]
@@ -63,10 +63,10 @@ namespace UnitTests.Helpers
             var myConverter = new StringEnumConverter();
 
             var myObject = new ItemModel();
-            var Result = myConverter.Convert(myObject, null, null, null);
-            var Expected = 0;
+            var result = myConverter.Convert(myObject, null, null, null);
+            var expected = 0;
 
-            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(expected, result, TestContext.CurrentContext.Test.Name);
         }
 
         // Convert Back
@@ -76,10 +76,10 @@ namespace UnitTests.Helpers
             var myConverter = new IntEnumConverter();
 
             var myObject = "Bogus";
-            var Result = myConverter.ConvertBack(myObject, null, null, null);
-            var Expected = 0;
+            var result = myConverter.ConvertBack(myObject, null, null, null);
+            var expected = 0;
 
-            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(expected, result, TestContext.CurrentContext.Test.Name);
         }
 
         // Convert Back
@@ -89,10 +89,10 @@ namespace UnitTests.Helpers
             var myConverter = new IntEnumConverter();
 
             var myObject = 40;
-            var Result = myConverter.ConvertBack(myObject, typeof(ItemLocationEnum), null, null);
-            var Expected = "Feet";
+            var result = myConverter.ConvertBack(myObject, typeof(ItemLocationEnum), null, null);
+            var expected = "Feet";
 
-            Assert.AreEqual(Expected, Result, TestContext.CurrentContext.Test.Name);
+            Assert.AreEqual(expected, result, TestContext.CurrentContext.Test.Name);
         }
     }
 }
