efc0e08e6ad5de2b49fb74b6f58ce667601ec0f9
Tate <tatebrasel@gmail.com>
2021-03-06 13:23:44 -0800

    Removed Mike's autobattle setup from AutoBattlePage.xaml.cs

--- Game/Game/Views/Home/AutoBattlePage.xaml.cs
+++ Game/Game/Views/Home/AutoBattlePage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -29,20 +29,6 @@ namespace Game.Views
 
 		public async void AutobattleButton_Clicked(object sender, EventArgs e)
 		{
-			// Call into Auto Battle from here to do the Battle...
-
-			// To See Level UP happening, a character needs to be close to the next level
-			var Character = new CharacterModel
-			{
-				ExperienceTotal = 300,    // Enough for next level
-				Name = "Mike Level Example",
-				Speed = 100,	// Go first
-			};
-
-			var CharacterPlayer = new PlayerInfoModel(Character);
-
-			BattleEngineViewModel.Instance.Engine.EngineSettings.CharacterList.Add(CharacterPlayer);
-
 			await AutoBattle.RunAutoBattle();
 			
 			var BattleMessage = string.Format("Done {0} Rounds", AutoBattle.Battle.EngineSettings.BattleScore.RoundCount);
