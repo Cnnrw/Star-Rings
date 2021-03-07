49b437ce0592c2110fc7434043fe5414a4a5efa9
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-07 11:49:57 -0800

    Update SafeFireAndForget function documentation
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Components/Utils.cs
+++ Game/Game/Components/Utils.cs
@@ -16,9 +16,12 @@ namespace Game
         /// async method from the constructor while communicating intent to
         /// fire and forget, and allow handling of exceptions.
         /// </summary>
-        /// <param name="task">Task to execute</param>
-        /// <param name="onException">What to do when method has an exception</param>
-        /// <param name="continueOnCapturedContext">If the context should be captured.</param>
+        /// <param name="task">Task.</param>
+        /// <param name="onException">If an exception is thrown in the Task, <c>onException</c> will execute. If onException is null, the exception will be re-thrown</param>
+        /// <param name="continueOnCapturedContext">
+        ///     If set to <c>true</c>, continue on captured context; this will ensure that the Synchronization context returns to the calling thread.
+        ///     If set to <c>false</c>, continue on a different context; this will allow the Synchronization context to continue on a different thread.
+        /// </param>
         public static async void SafeFireAndForget(this Task          task,
                                                    Action<Exception>? onException               = null,
                                                    bool               continueOnCapturedContext = false)
