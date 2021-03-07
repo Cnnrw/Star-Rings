736234abdb0f625c30077f75bfbe85170166b42f
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-07 11:50:16 -0800

    Add UTs for Utils.SafeFireAndForget function
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- UnitTests/Components/UtilsTests.cs
+++ UnitTests/Components/UtilsTests.cs
@@ -1,11 +1,56 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Threading.Tasks;
 
-namespace UnitTests.Views
+using Game;
+
+using NUnit.Framework;
+
+#nullable enable
+
+namespace UnitTests.Components
 {
     [TestFixture]
     public class UtilsTests
     {
+        const  int  Delay = 500;
+        static Task NoParameterTask() => Task.Delay(Delay);
 
+        static async Task NoParameterDelayedNullReferenceExceptionTask()
+        {
+            await Task.Delay(Delay);
+            throw new NullReferenceException();
+        }
+
+        [Test]
+        public async Task Utils_SafeFireAndForget_HandledException_Should_Pass()
+        {
+            // Arrange
+            Exception? exception = null;
+
+            // Act
+            NoParameterDelayedNullReferenceExceptionTask().SafeFireAndForget(ex => exception = ex);
+            await NoParameterTask();
+            await NoParameterTask();
+
+            // Assert
+            Assert.IsNotNull(exception);
+        }
+
+        [Test]
+        public async Task Utils_SafeFireAndForget_SetDefaultExceptionHandling_NoParams()
+        {
+            // Arrange
+            Exception? exception = null;
+            Action<Exception> myAct = ex => exception = ex;
+
+            // Call with Error = true
+
+            // Act
+            NoParameterDelayedNullReferenceExceptionTask().SafeFireAndForget(myAct);
+            await NoParameterTask();
+            await NoParameterTask();
+
+            Assert.IsNotNull(exception);
+        }
     }
 }
-
