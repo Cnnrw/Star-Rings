99ffba9d5e740cf67905cd8372c7b096dab78ef5
Jack Witt <jwitt1452@gmail.com>
2021-03-08 14:03:13 -0800

    Added app and page members to BattleHomePageTests

--- UnitTests/Views/Battle/BattleHomePageTests.cs
+++ UnitTests/Views/Battle/BattleHomePageTests.cs
@@ -17,6 +17,9 @@ namespace UnitTests.Views
     public class BattleHomePageTests : BattlePage
     {
 
+        private App app;
+        private BattlePage page;
+
         [SetUp]
         public void Setup()
         {
