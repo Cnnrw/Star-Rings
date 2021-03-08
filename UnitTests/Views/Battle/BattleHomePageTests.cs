0999daf075bb623bd8f4496e183eba43c616e345
Jack Witt <jwitt1452@gmail.com>
2021-03-08 14:26:26 -0800

    Added BattleHomePage_PickItems_Clicked_Default_Should_Pass to BattleHomePageTests

--- UnitTests/Views/Battle/BattleHomePageTests.cs
+++ UnitTests/Views/Battle/BattleHomePageTests.cs
@@ -94,5 +94,19 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void BattleHomePage_PickItems_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.PickItems_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
