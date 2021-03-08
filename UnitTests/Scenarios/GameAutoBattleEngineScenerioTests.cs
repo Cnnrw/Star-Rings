24f464767f01338f2ed20985ca06b4186670ddd9
Tate <tatebrasel@gmail.com>
2021-03-08 11:47:01 -0800

    Added GameAutoBattleEngineScenerioTests.cs

---
+++ UnitTests/Scenarios/GameAutoBattleEngineScenerioTests.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+
+using Game.Engine.EngineGame;
+using Game.Helpers;
+using Game.Models;
+
+using NUnit.Framework;
+
+namespace Scenario
+{
+    [TestFixture]
+    public class GameAutoBattleEngineScenarioTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            AutoBattle = new AutoBattleEngine();
+
+            AutoBattle.Battle.EngineSettings.CharacterList.Clear();
+            AutoBattle.Battle.EngineSettings.MonsterList.Clear();
+            AutoBattle.Battle.EngineSettings.CurrentDefender = null;
+            AutoBattle.Battle.EngineSettings.CurrentAttacker = null;
+
+            AutoBattle.Battle.StartBattle(true); // Clear the Engine
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        private AutoBattleEngine AutoBattle;
+
+    }
+}
