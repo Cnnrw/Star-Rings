4aaed48a7ddba8f30dacafd0e4181986913105ea
Tate <tatebrasel@gmail.com>
2021-03-08 12:54:43 -0800

    Added test AutoBattleEngine_RunAutoBattle_Monsters_1_Should_Pass to GameAutoBattleEngineScenerioTests.cs

--- UnitTests/Scenarios/GameAutoBattleEngineScenerioTests.cs
+++ UnitTests/Scenarios/GameAutoBattleEngineScenerioTests.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 
 using Game.Engine.EngineGame;
+using Game.Enums;
 using Game.Helpers;
 using Game.Models;
 
@@ -44,6 +45,65 @@ namespace Scenario
 
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public async Task AutoBattleEngine_RunAutoBattle_Monsters_1_Should_Pass()
+        {
+            // Arrange
+
+            // Set the round location
+
+            AutoBattle.Battle.EngineSettings.RoundLocation = BattleLocationEnum.Mordor;
+
+            // Add Characters
+
+            AutoBattle.Battle.EngineSettings.MaxNumberPartyCharacters = 1;
+
+            var CharacterPlayer = new PlayerInfoModel(
+                new CharacterModel
+                {
+                    Speed = -1,
+                    Level = 10,
+                    CurrentHealth = 100,
+                    ExperienceTotal = 1,
+                    ExperienceRemaining = 1,
+                    Name = "Luke",
+                    Job = CharacterJobEnum.Jedi,
+                    ListOrder = 1
+                }
+            );
+
+            AutoBattle.Battle.EngineSettings.CharacterList.Add(CharacterPlayer);
+
+            // Add Monsters
+
+            // Need to set the Monster count to 1, so the battle goes to Next Round Faster
+            AutoBattle.Battle.EngineSettings.MaxNumberPartyMonsters = 1;
+
+            var MonsterPlayer = new PlayerInfoModel(
+                new MonsterModel
+                {
+                    Speed = -1,
+                    Level = 1,
+                    CurrentHealth = 1,
+                    ExperienceTotal = 1,
+                    ExperienceRemaining = 1,
+                    Name = "Grog",
+                    BattleLocation = BattleLocationEnum.Mordor,
+                    ListOrder = 1
+                }
+            );
+
+            AutoBattle.Battle.EngineSettings.MonsterList.Add(MonsterPlayer);
+
+            //Act
+            var result = await AutoBattle.RunAutoBattle();
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(true, result);
         }
     }
 }
