42f514c600df4793124cf0f669a4b88f05b654a5
Jack Witt <jwitt1452@gmail.com>
2021-03-08 14:23:51 -0800

    Added  BattleHomePage_AutoBattle_Clicked_Default_Should_Pass to BattleHomePageTests

--- UnitTests/Views/Battle/BattleHomePageTests.cs
+++ UnitTests/Views/Battle/BattleHomePageTests.cs
@@ -66,5 +66,18 @@ namespace UnitTests.Views
             // Assert
             Assert.IsNotNull(result);
         }
+
+        public void BattleHomePage_AutoBattle_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.AutoBattle_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
