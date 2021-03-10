20ffcc31270beb919aa1915ab6fed92075f98fa7
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-09 18:57:15 -0800

    Refactor BasePage to BaseContentPage in RebelBasePage
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Views/RebelBase/RebelBasePage.xaml.cs
+++ Game/Game/Views/RebelBase/RebelBasePage.xaml.cs
@@ -1,27 +1,18 @@
-using Game.Services;
-using Game.Templates.Pages;
-using Game.ViewModels;
-
 namespace Game.Views
 {
     /// <summary>
     ///     The Main Game Page
     /// </summary>
-    public partial class RebelBasePage : BasePage
+    public partial class RebelBasePage : BaseContentPage
     {
         internal RebelBasePage(bool unitTests) { }
 
         /// <summary>
         ///     Constructor
         /// </summary>
-        public RebelBasePage() : this(App.NavigationService)
-        { }
-
-        public RebelBasePage(INavigationService navigationService)
+        public RebelBasePage()
         {
             InitializeComponent();
-
-            BindingContext = new RebelBaseViewModel(Navigation);
         }
     }
 }
