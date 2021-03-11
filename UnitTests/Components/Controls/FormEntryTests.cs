0155ab34d4279b7d7979d14483d5b034109ca5b7
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-10 19:26:38 -0800

    [wip] FormEntryTests.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- UnitTests/Components/Controls/FormEntryTests.cs
+++ UnitTests/Components/Controls/FormEntryTests.cs
@@ -1,10 +1,47 @@
+using Game;
+using Game.Controls;
+
+using Moq;
+
 using NUnit.Framework;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms.Xaml;
 
 namespace UnitTests.Components.Controls
 {
     [TestFixture]
     public class FormEntryTests
     {
-        
+        [SetUp]
+        public void SetUp()
+        {
+            MockForms.Init();
+
+            Application.Current = new App();
+        }
+
+        [TearDown]
+        public void TearDown() => Application.Current = null;
+
+        [Test]
+        public void FormEntry_LoadFromXaml_ShouldPass()
+        {
+            // Arrange
+            var formLbl = new FormEntry();
+
+            // Act
+            formLbl.LoadFromXaml("<FormEntry Text=\"Text\" />");
+
+            // Assert
+            Assert.AreEqual("Text", formLbl.Text);
+        }
+
+        [Test]
+        public void FormEntry_TextChanged_ShouldPass()
+        {
+            var mockFormLabel = new Mock<FormEntry>();
+        }
     }
 }
