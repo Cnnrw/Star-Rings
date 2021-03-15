c7beaecb87770a14a184956c491b11eaab06806c
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-15 13:41:09 -0700

    Create BattleHomePageViewModel.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

---
+++ Game/Game/Domain/ViewModels/Battle/BattleHomePageViewModel.cs
@@ -0,0 +1,37 @@
+using System.Windows.Input;
+
+using Game.Views;
+
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace Game.ViewModels
+{
+    public class BattleHomePageViewModel : BaseViewModel
+    {
+        public ICommand PickCharactersCommand =>
+            new AsyncCommand(() => NavigationService.NavigateAsync(nameof(PickCharactersPage)));
+
+        public ICommand PickItemsCommand =>
+            new AsyncCommand(() => NavigationService.NavigateAsync(nameof(PickItemsPage)));
+
+        public ICommand ScoresCommand =>
+            new AsyncCommand(() => NavigationService.NavigateAsync(nameof(ScorePage)));
+
+        public BattleHomePageViewModel() =>
+            ConfigureBattlePages();
+
+        void ConfigureBattlePages()
+        {
+            NavigationService.Configure(nameof(PickCharactersPage), typeof(PickCharactersPage));
+            NavigationService.Configure(nameof(BattlePage), typeof(BattlePage));
+            NavigationService.Configure(nameof(BattleSettingsPage), typeof(BattleSettingsPage));
+
+            NavigationService.Configure(nameof(NewRoundPage), typeof(NewRoundPage));
+            NavigationService.Configure(nameof(RoundOverPage), typeof(RoundOverPage));
+
+            NavigationService.Configure(nameof(PickItemsPage), typeof(PickItemsPage));
+
+            NavigationService.Configure(nameof(ScorePage), typeof(ScorePage));
+        }
+    }
+}
