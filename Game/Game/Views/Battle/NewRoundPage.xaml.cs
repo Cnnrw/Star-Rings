b3f01f1f3a72986469796a5aa23d3c4d2357d777
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-15 13:54:39 -0700

    Convert NewRoundPage to BaseContentPage
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Views/Battle/NewRoundPage.xaml.cs
+++ Game/Game/Views/Battle/NewRoundPage.xaml.cs
@@ -1,59 +1,56 @@
 using System;
-using System.Linq;
+using System.Diagnostics;
 
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
+using Game.Enums;
 using Game.Models;
 using Game.ViewModels;
-using Game.Enums;
-using System.Diagnostics;
+
+using Xamarin.Forms;
 
 namespace Game.Views
 {
-	/// <summary>
-	/// The Main Game Page
-	/// </summary>
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class NewRoundPage: ContentPage
-	{
-		// This uses the Instance so it can be shared with other Battle Pages as needed
-		public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+    /// <summary>
+    /// The Main Game Page
+    /// </summary>
+    public partial class NewRoundPage : BaseContentPage
+    {
+        // This uses the Instance so it can be shared with other Battle Pages as needed
+        readonly BattleEngineViewModel _engineViewModel = BattleEngineViewModel.Instance;
 
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		public NewRoundPage ()
-		{
-			InitializeComponent();
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public NewRoundPage()
+        {
+            InitializeComponent();
 
             string roundLocationName = BattleLocationEnumExtensions.ToMessageWithArticle(BattleEngineViewModel.Instance.Engine.EngineSettings.RoundLocation);
             RoundDetailsLabel.Text = "Your party is ambushed while traveling through " + roundLocationName + "!";
             Debug.WriteLine(BattleEngineViewModel.Instance.Engine.EngineSettings.RoundLocation);
             // Draw the Monsters
-            foreach (var data in EngineViewModel.Engine.EngineSettings.MonsterList)
-			{
-				MonsterListFrame.Children.Add(CreatePlayerDisplayBox(data));
-			}
-		}
+            foreach (var data in _engineViewModel.Engine.EngineSettings.MonsterList)
+            {
+                MonsterListFrame.Children.Add(CreatePlayerDisplayBox(data));
+            }
+        }
 
-		/// <summary>
-		/// Start next Round, returning to the battle screen
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		public async void BeginButton_Clicked(object sender, EventArgs e)
-		{
-			await Navigation.PopModalAsync();
-		}
+        /// <summary>
+        /// Start next Round, returning to the battle screen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public async void BeginButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
 
-		/// <summary>
-		/// Return a stack layout with the Player information inside
-		/// </summary>
-		/// <param name="data"></param>
-		/// <returns></returns>
-		public StackLayout CreatePlayerDisplayBox(PlayerInfoModel data)
-		{
+        /// <summary>
+        /// Return a stack layout with the Player information inside
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public StackLayout CreatePlayerDisplayBox(PlayerInfoModel data)
+        {
             if (data == null)
             {
                 data = new PlayerInfoModel();
@@ -69,13 +66,13 @@ namespace Game.Views
             };
 
             var ValueStyleMicro = Application.Current.Resources.TryGetValue("ValueStyleMicro", out object valueStyle1)
-                ? (Style)valueStyle1
-                : null;
+                                      ? (Style)valueStyle1
+                                      : null;
 
             // Add the Level
             var PlayerLevelLabel = new Label
             {
-                Text = "Level: "+data.Level,
+                Text = "Level: " + data.Level,
                 Style = ValueStyleMicro,
                 HorizontalOptions = LayoutOptions.Center,
                 HorizontalTextAlignment = TextAlignment.Center,
@@ -89,7 +86,7 @@ namespace Game.Views
             // Add the HP
             var PlayerHPLabel = new Label
             {
-                Text = "HP: "+ data.GetCurrentHealthTotal,
+                Text = "HP: " + data.GetCurrentHealthTotal,
                 Style = ValueStyleMicro,
                 HorizontalOptions = LayoutOptions.Center,
                 HorizontalTextAlignment = TextAlignment.Center,
@@ -108,9 +105,9 @@ namespace Game.Views
                 HorizontalTextAlignment = TextAlignment.Center,
                 Padding = 0,
                 LineBreakMode = LineBreakMode.TailTruncation,
-                CharacterSpacing=1,
-                LineHeight=1,
-                MaxLines =1,
+                CharacterSpacing = 1,
+                LineHeight = 1,
+                MaxLines = 1,
             };
 
             // Put the Image Button and Text inside a layout
@@ -123,7 +120,8 @@ namespace Game.Views
                 HorizontalOptions = LayoutOptions.Center,
                 Padding = 0,
                 Spacing = 0,
-                Children = {
+                Children =
+                {
                     PlayerImage,
                     PlayerNameLabel,
                     PlayerLevelLabel,
@@ -132,6 +130,6 @@ namespace Game.Views
             };
 
             return PlayerStack;
-		}
-	}
+        }
+    }
 }
