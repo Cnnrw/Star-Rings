4ff6a74b146ad72a4a9badeae59b31a643c3dc59
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-15 13:42:57 -0700

    Add BattleHomePageViewModel to BattleHomePage
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Views/Battle/BattleHomePage.xaml.cs
+++ Game/Game/Views/Battle/BattleHomePage.xaml.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Game.Views
 {
 	/// <summary>
@@ -10,39 +8,7 @@ namespace Game.Views
 		/// <summary>
 		/// Constructor
 		/// </summary>
-		public BattleHomePage () =>
-			InitializeComponent ();
-
-        /// <summary>
-        /// Navigate to the auto-battle page
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void AutoBattle_Clicked(object sender, EventArgs e) =>
-            await Navigation.PushAsync(new AutoBattlePage());
-
-        /// <summary>
-        /// Navigate to the pick characters page
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void PickCharacters_Clicked(object sender, EventArgs e) =>
-            await Navigation.PushAsync(new PickCharactersPage());
-
-        /// <summary>
-        /// Navigate to the new round page
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void PickItems_Clicked(object sender, EventArgs e) =>
-            await Navigation.PushAsync(new PickItemsPage());
-
-        /// <summary>
-        /// Navigate to the score page
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void ScorePage_Clicked(object sender, EventArgs e) =>
-            await Navigation.PushAsync(new ScorePage());
+		public BattleHomePage() =>
+			InitializeComponent();
     }
 }
