853edc33e6670243c81cbf3912888251a35502c9
Tate <tatebrasel@gmail.com>
2021-03-15 23:42:35 -0700

    Added ToImageURI tests to MonsterImageEnumExtensionsTests.cs

--- UnitTests/Domain/Enum/MonsterImageEnumExtensionsTests.cs
+++ UnitTests/Domain/Enum/MonsterImageEnumExtensionsTests.cs
@@ -132,5 +132,131 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual("Warg Rider", result);
         }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.Unknown.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("item.png", result);
+        }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_DarkElf_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.DarkElf.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("dark_elf.png", result);
+        }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_DeadKing_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.DeadKing.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("dead_king.png", result);
+        }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_Nazgul_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.Nazgul.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("nazgul.png", result);
+        }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_Oliphant_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.Oliphant.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("oliphant.png", result);
+        }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_Orc_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.Orc.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("orc.png", result);
+        }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_Spider_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.Spider.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("spider.png", result);
+        }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_Troll_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.Troll.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("troll.png", result);
+        }
+
+        [Test]
+        public void MonsterImageEnumExtensionsTests_ToImageURI_WargRider_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterImageEnum.WargRider.ToImageURI();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("warg_rider.png", result);
+        }
     }
 }
