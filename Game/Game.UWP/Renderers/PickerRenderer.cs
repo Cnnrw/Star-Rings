e843c6a4d2afb19b024600b6384ace34ac507d8e
Connor <18753272+Cnnrw@users.noreply.github.com>
2021-03-17 13:28:17 -0700

    Add UWP Picker renderer

---
+++ Game/Game.UWP/Renderers/PickerRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Picker), typeof(Game.UWP.Renderers.PickerRenderer))]
+namespace Game.UWP.Renderers
+{
+    public class PickerRenderer : Xamarin.Forms.Platform.UWP.PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.BorderThickness = new Thickness(0,0,0,1);
+
+            Control.Margin = new Thickness(0);
+            Control.Padding = new Thickness(0);
+
+            Control.Background = new SolidColorBrush(Colors.Transparent);
+        }
+    }
+}
