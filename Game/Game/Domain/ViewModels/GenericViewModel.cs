3743c4c23f738d1d08031b531d8559db37564f4c
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-16 18:45:11 -0700

    Add Update/Delete commands to GenericViewModel.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Domain/ViewModels/GenericViewModel.cs
+++ Game/Game/Domain/ViewModels/GenericViewModel.cs
@@ -1,11 +1,38 @@
-﻿using Game.Models;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Game.Models;
 using Game.Services;
+using Game.Views;
+
+using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace Game.ViewModels
 {
     public class GenericViewModel<T> : BaseViewModel
         where T : class
     {
+        public ICommand UpdateCommand { get; } =
+            new AsyncCommand<GenericViewModel<T>>(OnUpdateClicked);
+
+        public ICommand DeleteCommand { get; } =
+            new AsyncCommand<GenericViewModel<T>>(OnDeleteClicked);
+
+        static Task OnUpdateClicked(GenericViewModel<T> viewModel) =>
+            viewModel switch
+            {
+                GenericViewModel<CharacterModel> _ => NavigationService.NavigateModalAsync(nameof(CharacterUpdatePage), viewModel),
+                _ => throw new ArgumentOutOfRangeException(nameof(viewModel), viewModel, null)
+            };
+
+        static Task OnDeleteClicked(GenericViewModel<T> viewModel) =>
+            viewModel switch
+            {
+                GenericViewModel<CharacterModel> _ => NavigationService.NavigateModalAsync(nameof(CharacterDeletePage), viewModel),
+                _ => throw new ArgumentOutOfRangeException(nameof(viewModel), viewModel, null)
+            };
+
         #region Ctors
 
         /// <summary>
