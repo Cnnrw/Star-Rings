4d714432f504d6212a6aa630731cbe65c886f9c5
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-16 18:51:59 -0700

    Update event handlers to use App.NavigationService in CharacterDeletePage.xaml.cs
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Views/RebelBase/Characters/CharacterDeletePage.xaml.cs
+++ Game/Game/Views/RebelBase/Characters/CharacterDeletePage.xaml.cs
@@ -40,7 +40,7 @@ namespace Game.Views
         public async void Delete_Clicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "Delete", _viewModel.Data);
-            await Navigation.PopModalAsync();
+            await App.NavigationService.GoBack();
         }
 
         /// <summary>
@@ -48,7 +48,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        public async void Cancel_Clicked(object sender, EventArgs e) => await Navigation.PopModalAsync();
+        public async void Cancel_Clicked(object sender, EventArgs e) => await App.NavigationService.GoBack();
 
         /// <summary>
         ///     Override back button behavior to capture user on screen
