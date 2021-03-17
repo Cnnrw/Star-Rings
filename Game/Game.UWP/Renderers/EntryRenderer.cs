fddee1502ff687626c7787ca4898f38a75b248a1
Connor <18753272+Cnnrw@users.noreply.github.com>
2021-03-17 13:27:55 -0700

    Add UWP Entry renderer

---
+++ Game/Game.UWP/Renderers/EntryRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Entry), typeof(Game.UWP.Renderers.EntryRenderer))]
+namespace Game.UWP.Renderers
+{
+    public class EntryRenderer : Xamarin.Forms.Platform.UWP.EntryRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+                return;
+
+            Control.Background = new SolidColorBrush(Colors.Transparent);
+            Control.BackgroundFocusBrush = new SolidColorBrush(Colors.Transparent);
+
+            Control.BorderBrush = new SolidColorBrush(Colors.Black);
+            Control.BorderThickness = new Thickness(0,0,0,1);
+
+            Control.VerticalAlignment = VerticalAlignment.Center;
+        }
+    }
+}
