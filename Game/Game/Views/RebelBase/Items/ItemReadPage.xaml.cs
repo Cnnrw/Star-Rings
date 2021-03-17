f97bd2ae34feab90641df5c1c3cdd945af55f51e
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-03-16 19:21:56 -0700

    Add commands to ItemReadPage
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- Game/Game/Views/RebelBase/Items/ItemReadPage.xaml.cs
+++ Game/Game/Views/RebelBase/Items/ItemReadPage.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-
-using Game.Models;
+﻿using Game.Models;
 using Game.ViewModels;
-
-using Xamarin.Forms;
 
 namespace Game.Views
 {
@@ -12,9 +8,6 @@ namespace Game.Views
     /// </summary>
     public partial class ItemReadPage : BaseContentPage
     {
-        // View Model for Item
-        readonly GenericViewModel<ItemModel> _viewModel;
-
         // Empty Constructor for UTs
         internal ItemReadPage(bool unitTest) { }
 
@@ -27,30 +20,6 @@ namespace Game.Views
         public ItemReadPage(GenericViewModel<ItemModel> data)
         {
             InitializeComponent();
-
-            BindingContext = _viewModel = data;
-        }
-
-        /// <summary>
-        /// Save calls to Update
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void Update_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage(new ItemUpdatePage(_viewModel)));
-            await Navigation.PopAsync();
-        }
-
-        /// <summary>
-        /// Calls for Delete
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void Delete_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(_viewModel)));
-            await Navigation.PopAsync();
         }
     }
 }
